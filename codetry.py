a0c24a01412272fd6b8bc9a815d16c7539f893eb
vaggelis kalabokis <vaggeliskg@gmail.com>
2023-12-21 18:49:49 +0200

    ffs

--- codetry.py
+++ codetry.py
@@ -31,25 +31,22 @@ def read_dom_file(rlfap_folder_path, file_name_dom):
     dom_file_path = os.path.join(rlfap_folder_path, file_name_dom)
     dom_dict = {}
     rests = []
-
-    var_dict = read_var_file(rlfap_folder_path, 'var2-f24.txt')  # Get the dictionary of variables and indicators
-    
     with open(dom_file_path, 'r') as file:
         dom_lines = file.readlines()[1:]  # Skip the first line
 
         for line in dom_lines:
-            first_number,rest = line.split(maxsplit=1)  # Split the line into individual numbers
-            rests.append(rest)
-
+            first_number, rest = line.split(maxsplit=1)  # Split the line into individual numbers
+            rests.append(list(map(int, rest.strip().split())))  # Split values into integers and store as lists
+    
     var_file_path = os.path.join(rlfap_folder_path, 'var2-f24.txt')
     with open(var_file_path, 'r') as file:
         lines = file.readlines()[1:]  # Skip the first line
         for line in lines:
             first_num, second_num = line.split()
-            if second_num == first_number[0]: 
-                dom_dict[first_num] = {rests[0]}
+            if second_num == '0':
+                dom_dict[first_num] = rests[0]  # Assign the split values directly for keys with '0'
             else:
-                dom_dict[first_num] = {rest[1]}
+                dom_dict[first_num] = rests[1]  # Assign the split values directly for keys with '1'
 
     return dom_dict
 
